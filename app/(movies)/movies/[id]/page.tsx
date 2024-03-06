b5e6b715d5351e2af8e82b8b2b54a4254a1e1b2b
qkrtjddms587 <109716738+qkrtjddms587@users.noreply.github.com>
2024-03-06 15:15:04 +0900

    4.0

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        {/* @ts-expect-error Async Server Component */}
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        {/* @ts-expect-error Async Server Component */}
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
